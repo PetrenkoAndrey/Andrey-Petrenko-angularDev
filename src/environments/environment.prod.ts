a6f90bf0980c9855c8787f3f1d19cda6192fee50
Andrey Petrenko <apetrenko@deepcode.co>
2021-12-28 16:38:10 +0200

    config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_URI: 'http://dataservice.accuweather.com',
-  API_KEY: 'http://dataservice.accuweather.com'
+  API_KEY: '00Fnn9QIUGbyYnV2NLZDNZMmaaTpMnyM'
 };
